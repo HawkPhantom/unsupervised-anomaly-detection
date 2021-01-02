747fafa6e5d260bceb4f5bde488620a2c471d501
Furkan Ciylan <58641429+HawkPhantom@users.noreply.github.com>
2021-01-02 10:17:18 +0300

    bug fix

--- anomaly_detection.py
+++ anomaly_detection.py
@@ -1,16 +1,15 @@
-# -*- coding: utf-8 -*-
 ##Imports
 import pandas as pd
 import subprocess
 import sys
- 
+
 try:
     from adtk.data import validate_series
     from adtk.visualization import plot
     from adtk.detector import PersistAD
     from adtk.transformer import DoubleRollingAggregate
     from adtk.pipe import Pipeline
- 
+
 except:
     subprocess.check_call([sys.executable, "-m", "pip", "install", "adtk"])
     from adtk.data import validate_series
@@ -19,8 +18,15 @@ except:
     from adtk.transformer import DoubleRollingAggregate
     from adtk.pipe import Pipeline
 
-from google.colab import files
-uploaded = files.upload()
+##############################################################################################################
+
+##Import and Validate the Dataset
+s_train = pd.read_csv("./cagri_I.csv", index_col="timestamp", parse_dates=True, squeeze=True)
+s_train = validate_series(s_train)
+print(s_train)
+plot(s_train) ##plot Function Draws a Chart but The Chart Is in JPEG Format !!!
+
+##############################################################################################################
 
 ##PersistAD Detects Spikes (Extremely Abnormal Values)
 ##High Tolerance Model
@@ -32,6 +38,8 @@ for i in anomalies_1:
     print(i)
 print(len(anomalies_1))
 
+##############################################################################################################
+
 ##Pipeline Definition to Combine PersistAD Detector with Double Rolling Aggregate Transformer
 ##Low Tolerance Model
 steps = [
@@ -40,6 +48,8 @@ steps = [
 ]
 
 pipeline = Pipeline(steps)
+
+##############################################################################################################
 
 ##Training Phase
 anomalies_2 = pipeline.fit_detect(s_train,return_list=True) ##fit_detect Function Trains the Model and Detects Anomalies in Training Set
@@ -50,6 +60,8 @@ for i in anomalies_2:
 
 print(len(anomalies_2))
 
+##############################################################################################################
+
 ##Take difference between high tolerance and low tolerance modelds
 def Diff(li1, li2): 
     return (list(set(li1) - set(li2))) 
@@ -57,16 +69,17 @@ def Diff(li1, li2):
 li1 = anomalies_1
 li2 = anomalies_2 
 
-##How li1 differs from li2
+##How li1 Differs From li2
 a = list(set(li1) - set(li2))
 
 for i in a:
     print(i) 
 
-##Combine the results of low tolerance and high tolerance models
+##Combine the Results of Low Tolerance and High Tolerance Models
 for i in a:
     li2.append(i)
 
+##############################################################################################################
 
 a = list(set(li1) - set(li2))
 
@@ -75,7 +88,9 @@ for i in a:
 
 print(len(a))
 
-##Combined results of low and high tolerance models
+##############################################################################################################
+
+##Combined Results of Low and High Tolerance Models
 for i in li2:
     print(i)
 print(len(li2))
